e9cdb33c11b19e9267f91c221cc5408d5ad3ad8c
Teodora Mihoc <teodora.mihoc@canonical.com>
2025-07-28 11:33:43 +0200

    docs: set up google analytics, fix static path

--- conf.py
+++ conf.py
@@ -154,7 +154,7 @@ slug = 'jaas'
 
 # Template and asset locations
 
-html_static_path = ["_static"]
+html_static_path = [".sphinx/_static"]
 templates_path = [".sphinx/_templates"]
 
 #######################
@@ -289,15 +289,15 @@ exclude_patterns.extend(
     ],
 )
 
-# Adds custom CSS files, located under 'html_static_path'
+# Add CSS files (located in .sphinx/_static/)
+html_css_files = [
+	'css/cookie-banner.css'
+]
 
-# html_css_files = []
-
-
-# Adds custom JavaScript files, located under 'html_static_path'
-
-# html_js_files = []
-
+# Add JavaScript files (located in .sphinx/_static/)
+html_js_files = [
+	'js/bundle.js',
+]
 
 # Specifies a reST snippet to be appended to each .rst file
 
